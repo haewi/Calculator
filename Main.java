e4ca94c72c38b78476594a9256f8fd6722471d3b
Yeahn <21900151@handong.edu>
2020-07-11 14:40:23 +0900

    commit some files

---
+++ Main.java
@@ -0,0 +1,19 @@
+package edu.handong.round2;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JMenuBar m = new JMenuBar();
+		JFrame f = new JFrame();
+//		f.add
+//		ChangeEvent c = new ChangeEvent();
+		Canvas cx = new Canvas();
+//		cx.
+	}
+
+}
